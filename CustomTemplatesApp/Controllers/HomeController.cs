89452e0df1c9d6b908f49f861b76aa67e81216a5
Roman Tyshchenko <tishhenko.r@galaktika.ru>
2017-09-22 01:17:38 +0300

    Display and editing templates were added

--- CustomTemplatesApp/Controllers/HomeController.cs
+++ CustomTemplatesApp/Controllers/HomeController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Шаблоны представлений, создаваемых с помощью пункта контекстного меню "Add view..." хранятся в директории:
+D:\Microsoft\Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Web\Mvc\Scaffolding\Templates\MvcView
+Менять шаблоны в этой директории не рекомендуется. Измененные шаблоны рекомендуется размещать в папке
+проекта CodeTemplates.
+*/
+using CustomTemplatesApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +18,33 @@ namespace CustomTemplatesApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult MyTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            Book book = new Book { Name = "Война и мир", Id = 1, Price = 23.8m };
+            return View(book);
+        }
+
+        public ActionResult Display()
+        {
+            Book book = new Book { Name = "Война и мир", Id = 1, Price = 23.8m };
+            return View(book);
         }
 
         public ActionResult About()
